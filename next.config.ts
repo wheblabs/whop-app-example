a020e3ea06511cf433526ecba1fe6eec580eddc2
brentably <brentably@gmail.com>
2025-06-13 14:58:49 -0700

    lets go

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,48 @@
 import type { NextConfig } from "next";
 
+// I'm not sure what a lot of this does -- but it might work!
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
 		remotePatterns: [{ hostname: "**" }],
+	},
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: "/_next/webpack-hmr",
+	// 			destination: "https://SPECIFICSUBDOMAIN.vidgenx.com/_next/webpack-hmr",
+	// 		},
+	// 	];
+	// },
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "X-Frame-Options",
+						value: "SAMEORIGIN",
+					},
+					{
+						key: "Content-Security-Policy",
+						value: "frame-ancestors 'self' *.vidgenx.com",
+					},
+				],
+			},
+			{
+				source: "/_next/webpack-hmr",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "*",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, OPTIONS",
+					},
+				],
+			},
+		];
 	},
 };
 
