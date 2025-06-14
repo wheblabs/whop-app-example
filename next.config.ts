630bc5e7a1dc5e29fb0c31262fcaf88019dff295
Constantine <konstantinbelenko36@gmail.com>
2025-06-13 18:33:07 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	webpackDevMiddleware: config => {
+    		config.watchOptions = {
+      			poll: 1000,
+      			aggregateTimeout: 300,
+    		}
+    		return config
+	},
 	images: {
 		remotePatterns: [{ hostname: "**" }],
 	},
