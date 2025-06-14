688f86a309326b73c682ba4dc3b2dec2bd6d43a4
Constantine <konstantinbelenko36@gmail.com>
2025-06-13 18:41:39 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	webpackDevMiddleware: config => {
-    		config.watchOptions = {
-      			poll: 1000,
-      			aggregateTimeout: 300,
-    		}
-    		return config
-	},
 	logging: {
 		fetches: {
 			fullUrl: true,
