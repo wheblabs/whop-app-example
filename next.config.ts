6182dccbc2a93fab1b869d7c50147f3c74aa7241
ConstantineB6 <konstantinbelenko36@gmail.com>
2026-02-19 23:44:07 -0800

    Remove PostHog from template app
    - Delete instrumentation-client.ts
    - Remove posthog-js from package.json
    - Remove /ingest rewrites from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -76,19 +76,7 @@ const nextConfig: NextConfig = {
 			},
 		];
 	},
-	async rewrites() {
-		return [
-			{
-				source: "/ingest/static/:path*",
-				destination: "https://us-assets.i.posthog.com/static/:path*",
-			},
-			{
-				source: "/ingest/:path*",
-				destination: "https://us.i.posthog.com/:path*",
-			},
-		];
-	},
-	skipTrailingSlashRedirect: true,
+skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
