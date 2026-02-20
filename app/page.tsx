19f6f181be2abd907c912fe67d96c766004962d9
ConstantineB6 <konstantinbelenko36@gmail.com>
2026-02-20 00:02:05 -0800

    Fix page.tsx centering flash: use inline styles, drop 'use client'

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,13 @@
-"use client";
-
 import { Heading, Text, Spinner } from "@whop/react/components";
-import styled from "styled-components";
-
-const Center = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	min-height: 100vh;
-	gap: 16px;
-`;
 
 export default function Page() {
 	return (
-		<Center>
+		<div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh", gap: "16px" }}>
 			<Spinner size="3" />
 			<Heading size="4">Building your app...</Heading>
 			<Text color="gray" size="2">
 				Describe what you want in the chat
 			</Text>
-		</Center>
+		</div>
 	);
 }
